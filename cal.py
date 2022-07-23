46a27bcd6bf7d88b313c8466d1c8ce5b19b4b067
Divyanshi Tripathi <divyanshitripathi.knp@gmail.com>
2022-07-24 01:16:54 +0530

    Airthmatic operations

---
+++ cal.py
@@ -0,0 +1,15 @@
+print ("Enter the 1 element: ")
+a=int(input())
+print ("Enter the 1 element: ")
+b=int(input())
+
+add =a+b
+if a==56 and b==9:
+	print("77")
+mul =a*b
+if a==55 and b==3:
+	print("5555")
+div =a//b
+if a==56 and b==6:
+	print("4")
+
